23be7c4fc8e84c0a95918b9251d450eae14cbac2
Nishaam86 <82016868+op-tusk@users.noreply.github.com>
2026-04-04 20:35:54 +0530

    Add files via upload

---
+++ src/main/java/com/nef/notenoughfakepixel/utils/PetData.java
@@ -0,0 +1,21 @@
+package com.nef.notenoughfakepixel.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PetData {
+    public static final List<String> ALL_PETS = Arrays.asList(
+            "Ammonite", "Armadillo",
+            "Baby Yeti", "Bal", "Bat", "Bee", "Black Cat", "Blaze", "Blue Whale",
+            "Chicken", "Dolphin", "Developer's Pet" ,
+            "Duck" ,
+            "Frozen Bubble" ,
+            "Relic Keeper" , "Droplet wisp" ,
+            "Elephant", "Ender Dragon", "Enderman", "Endermite",
+            "Flying Fish", "Ghoul", "Giraffe", "Golem", "Golden Dragon", "Griffin", "Guardian",
+            "Horse", "Jerry", "Lion", "Megalodon", "Mithril Golem", "Monkey",
+            "Mooshroom Cow", "Ocelot", "Parrot", "Phoenix", "Pigman",
+            "Rabbit", "Rock", "Sheep", "Silverfish", "Skeleton Horse", "Snowman",
+            "Spider", "Squid", "Tarantula", "Tiger", "Wither Skeleton", "Wolf", "Zombie"
+    );
+}
